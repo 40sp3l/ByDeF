a48e903835da2e541271b6e11807223702f1f35e
40sp3l <gospelchukwunonso@proton.me>
2025-05-12 11:21:17 +0100

    Delete bydef_compile.py

--- bydef_compile.py
+++
@@ -1,4 +0,0 @@
-from subprocess import check_output
-import subprocess
-check_output('pyinstaller --noconsole --onefile bydef_enc.py')
-print('[+] Compiled successfully... locate the PE inside the dist folder... ta-da')
